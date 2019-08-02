f8064d606172e1ad249115141a42d084699b46c6
JJ Merelo <jjmerelo@gmail.com>
2019-08-02 23:03:25 +0200

    Advances slides

--- data/survey.R
+++ data/survey.R
@@ -4,3 +4,5 @@ library("ggthemes")
 data <- read.csv("perl6-doc-survey.csv",sep=';')
 data$Year <- as.factor(data$Year)
 data$Score <- as.numeric(data$Score)
+
+ ggplot(data, aes(x=Year,y=Score))+ geom_boxplot()
